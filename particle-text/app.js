b48aab7243bd6c7f2ff67aa6f83aff420377f824
VladykoD <VladykoD@gmail.com>
2020-12-28 23:31:25 +0300

    text and particles, full animation

--- particle-text/app.js
+++ particle-text/app.js
@@ -7,10 +7,12 @@
 
    let particleArray = [];
 
+   const dotColor = '#C8E0EE'
+
    let mouse = {
       x: null,
       y: null,
-      radius: 250,
+      radius: 150,
    }
 
    window.addEventListener('mousemove', function(event) {
@@ -18,28 +20,29 @@
       mouse.y = event.y;
    })
 
-   ctx.fillStyle = '#ffc93c'
-   ctx.font = '700  120px / 1 "Arial Black"';
-   ctx.fillText('Hello, sunshine!', 200, 400);
+   ctx.fillStyle = dotColor;
+   ctx.font = '700  20px "Arial Black"';
+   ctx.fillText('HI!', 10, 20);
 
-   const textCoordinates = ctx.getImageData(100, 200, 1200, 300);
+   const textCoordinates = ctx.getImageData(0, 0, 300, 300);
 
    class Particle {
       constructor(x, y) {
          this.x = x;
          this.y = y;
-         this.size = 5;
+         this.size = 20;
          this.baseX = this.x;
          this.baseY = this.y;
-         this.density = (Math.random() * 10) + 30;
+         this.density = (Math.random() * 200) ;
       }
 
       draw() {
-         ctx.fillStyle = '#ffc93c';
+         ctx.strokeStyle = dotColor;
+         ctx.lineWidth = 4;
          ctx.beginPath();
          ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
          ctx.closePath();
-         ctx.fill();
+         ctx.stroke();
       }
 
       update() {
@@ -55,17 +58,17 @@
          let directionY = force * forceDirectionY * this.density;
 
          if(distance < mouse.radius) {
-            this.x -= directionX * 2
-            this.y -= directionY * 2
+            this.x -= directionX * 4
+            this.y -= directionY * 4
          } else {
             if (this.x !== this.baseX) {
                let dx = this.x - this.baseX;
-               this.x -= dx/10;
+               this.x -= dx/60;
             }
 
             if (this.y !== this.baseY) {
                let dy = this.y - this.baseY;
-               this.y -= dy/10;
+               this.y -= dy/60;
             }
          }
       }
@@ -74,10 +77,14 @@
    function init() {
       particleArray = [];
 
-      for(let i = 0; i < 1000; i++) {
-         let x = Math.random() * w
-         let y = Math.random() * h
-         particleArray.push(new Particle(x, y));
+      for (let y = 0, y2 = textCoordinates.height; y < y2; y++) {
+         for (let x = 0, x2 = textCoordinates.width; x < x2; x++) {
+            if (textCoordinates.data[(y * 4 * textCoordinates.width) + (x * 4) + 3] > 128) {
+               let positionX = x;
+               let positionY = y ;
+               particleArray.push(new Particle(positionX * 60, positionY * 60))
+            }
+         }
       }
    }
    init();
